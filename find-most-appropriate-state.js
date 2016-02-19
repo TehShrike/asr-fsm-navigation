bb4a991fff3e2d5266d3f14d103d90a6332359ba
Josh Duff <me@JoshDuff.com>
2016-02-18 19:13:07 -0600

    Making the findMostAppropriateState function a bit cleaner

--- find-most-appropriate-state.js
+++ find-most-appropriate-state.js
@@ -1,7 +1,7 @@
 const allShortNames = require('./all-short-names')
 
 module.exports = function findMostAppropriateState(fsmStateOptions, stateName) {
-	return allShortNames(stateName).reduce((memo, current) => {
-		return fsmStateOptions.indexOf(current) === -1 ? memo : current
-	}, null)
+	return allShortNames(stateName)
+		.reverse()
+		.find(state => fsmStateOptions.indexOf(state) !== -1) || null
 }
